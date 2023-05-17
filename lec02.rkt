2eab6ef4090508fe8701f47042e4fb183f50a5f9
syn <yuncong.wang@gmail.com>
2023-05-17 21:10:33 +0800

    add sum-int and sum-square function

--- lec02.rkt
+++ lec02.rkt
@@ -1,2 +1,15 @@
 #lang racket
+(require "base.rkt")
 (require racket/trace)
+
+(define sum-int
+  (lambda (a b)
+    (cond
+      ((> a b) 0)
+      (else (+ a (sum-int (+ 1 a) b))))))
+
+(define sum-square
+  (lambda (a b)
+    (cond
+      ((> a b) 0)
+      (else (+ (square a) (sum-square (+ 1 a) b))))))
