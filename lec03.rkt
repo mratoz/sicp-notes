a05064ee5af90d492c7d34bbec9acdc6ea3b7578
syn <yuncong.wang@gmail.com>
2023-06-27 21:38:56 +0800

    lecture 03 notes initial: vector op, list, scale

---
+++ lec03.rkt
@@ -0,0 +1,41 @@
+#lang racket
+(require "base.rkt")
+(require racket/trace)
+
+(define +vect
+  (lambda (v1 v2)
+    (make-vector
+     (+ (xcor v1) (xcor v2))
+     (+ (ycor v1) (ycor v2)))))
+
+
+;;(define v1 (make-vector 1 2))
+;;(define v2 (make-vector 3 4))
+;;(+vect v1 v2)
+
+(define scale
+  (lambda (s v)
+    (make-vector
+     (* s (xcor v))
+     (* s (ycor v)))))
+
+;;(define v1 (make-vector 1 2))
+;;(scale 2 v1)
+
+;;list
+(define 1-to-4 (list 1 2 3 4))
+;;(car 1-to-4)
+;;(car (cdr 1-to-4))
+;;(car (cdr (cdr 1-to-4)))
+;;(car (cdr (cdr (cdr 1-to-4))))
+;;error
+;;(car (cdr (cdr (cdr (cdr 1-to-4)))))
+;;(cdr (cdr (cdr (cdr 1-to-4))))
+;;error
+;;(cdr (cdr (cdr (cdr (cdr 1-to-4)))))
+
+(define scale-list
+  (lambda (s l)
+    (cond
+      ((null? l) '())
+      (else (cons (* s (car l)) (scale-list s (cdr l)))))))
