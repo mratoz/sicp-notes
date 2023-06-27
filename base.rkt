89d3cc2cfbc24437c1042bc83521a76ecdf0f1af
syn <yuncong.wang@gmail.com>
2023-06-27 21:39:56 +0800

    update base for lecture 03

--- base.rkt
+++ base.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (provide square average)
+(provide make-vector xcor ycor)
 
 (define square
   (lambda (x)
@@ -7,3 +8,36 @@
 (define average
   (lambda (x y)
     (/ (+ x y) 2)))
+
+;;lec2b: vector
+(define make-vector
+  (lambda (x y)
+    (cons x (cons y '()))))
+
+(define xcor
+  (lambda (p)
+    (car p)))
+(define ycor
+  (lambda (p)
+    (car (cdr p))))
+
+(define make-seg
+  (lambda (p q)
+    (cons p (cons q '()))))
+(define seg-start
+  (lambda (s)
+    (car s)))
+(define seg-end
+  (lambda (s)
+    (car (cdr s))))
+
+(define mid-point
+  (lambda (s)
+    (let ([a (seg-start s)] [b (seg-end s)])
+      (make-vector (average (xcor a) (xcor b)) (average (ycor a) (ycor b))))))
+
+(define length
+  (lambda (s)
+    (let ([dx (- (xcor (seg-end s)) (xcor (seg-start s)))]
+          [dy (- (ycor (seg-end s)) (ycor (seg-start s)))])
+      (sqrt (+ (square dx) (square dy))))))
